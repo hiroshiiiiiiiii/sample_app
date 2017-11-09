4390ef568d05092630f61435b8ccb9830ce8df36
Hiroshi Akagi <hiroshidesu.1@gmail.com>
2017-11-09 20:39:40 +0900

    Added Login function part1

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -7,6 +9,10 @@ Rails.application.routes.draw do
    get  '/about',   to: 'static_pages#about'
    get  '/contact', to: 'static_pages#contact'
    get  '/signup',  to: 'users#new'
+
+   get  '/login',  to: 'sessions#new'
+   post  '/login',  to: 'sessions#create'
+   get  '/logout',  to: 'sessions#destroy'
 
    resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
